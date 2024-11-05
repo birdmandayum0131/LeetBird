fe929f40bfe0164c23deec4a163139a1f7e5e8a7
Bird <birdmandayum0131@gmail.com>
2024-11-05 18:38:26 +0800

    dp : draft question 264

---
+++ DP/(Medium)264_Ugly_Number_II/Solution_1(0ms).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+using namespace std;
+class Solution
+{
+  public:
+    int nthUglyNumber(int n)
+    {
+
+    }
+};
+int main(void)
+{
+    Solution solution;
+    int n = 10;
+    int output = solution.nthUglyNumber(n);
+    cout << output << endl;
+}

2050fd782ec8b9e956bf8e4ad8d155d2546dc592
Bird <birdmandayum0131@gmail.com>
2024-11-26 15:07:07 +0800

    dag : solve daily question 2924

---
+++ Linked_List/(Medium)2924_Find_Champion_II/Solution_1(41ms).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+  public:
+    int findChampion(int n, vector<vector<int>> &edges)
+    {
+        vector<short> node_map(n);
+        for (auto edge : edges)
+            node_map[edge[1]] = edge[0] + 1;
+
+        int champion = -1;
+        int champion_count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (node_map[i] == 0)
+            {
+                champion = i;
+                champion_count++;
+            }
+        }
+    }
+};
+
+int main(void)
+{
+    Solution solution;
+    int n = 4;
+    vector<vector<int>> edges = {{0, 2}, {1, 3}, {1, 2}};
+    int result = solution.findChampion(n, edges);
+    cout << result << endl;
+}
